5474cd2257d1ebe065088e3e29419eed57a2e7b4
Sandhiya S <120866300+Sandhiya-S67@users.noreply.github.com>
2025-04-29 16:44:02 +0530

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyBI9A8wp9HnQhFUz14QyRU3HOKiO92G95Q");
+const genAI = new GoogleGenerativeAI("");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const COACHING_CONTEXT = `
